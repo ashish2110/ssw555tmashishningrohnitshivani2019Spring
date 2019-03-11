ef7515a03d50a4842a9a70952b1edb17de9e7e1b
Rohnit Shetty <rshetty6@stevens.edu>
2019-03-11 09:48:21 -0400

    travis added

--- us_rs.py
+++ us_rs.py
@@ -1,12 +1,14 @@
 class us_rs:
     
+    def printError(lineNumber, UserstoryName, message):
+        print("ERROR " + UserstoryName + " in line " + str(lineNumber) + ". " + message )
+
     # UserStory 15
     def siblingCount(family):
         validity = True
         if(type(family) is not (dict)):
             print("Argument isn't an dict")
             validity = False
-            return validity
         
         for x, values in family.items():
             
@@ -16,15 +18,11 @@ class us_rs:
                 length = 0
             if length > 15:
                 validity = False
-                print("ERROR US15 in line " + str(values['CHIL'][0][1]) + " : There are more than 15 siblings for family. " )
-                return validity
-
-          
+                us_rs.printError(values['CHIL'][0][1], "US 15", "There are more than 15 siblings for family.")
+                         
         return validity
 
-
     #Userstory 6 - Divorce can only occur before death of both spouses
-
     def divorceBeforeDeath(individual, family):
         flag = True
         error = []
@@ -32,26 +30,18 @@ class us_rs:
             divorceDate = famvalue['DIV_DATE']
             if divorceDate == "NA":
                 flag = True
-                continue
-            divorceDate = divorceDate[0]
-            husband = famvalue['HUSB'][0]
-            husbandDeath = individual[husband]['DEAT_DATE']
-            wife = famvalue['WIFE'][0]
-            wifeDeath = individual[wife]['DEAT_DATE']
-              
-            if(husbandDeath[0] < divorceDate):
+                continue 
+            husbandDeath = individual[famvalue['HUSB'][0]]['DEAT_DATE']
+            wifeDeath = individual[famvalue['WIFE'][0]]['DEAT_DATE']  
+            if(husbandDeath[0] < divorceDate[0]):
                 error.append(str(husbandDeath[1]))
                 flag = False
-            
-            print(wife)
-            print(wifeDeath)
-            if(wifeDeath[0] < divorceDate):
+            if(wifeDeath[0] < divorceDate[0]):
                 error.append(str(wifeDeath[1]))
-                print(error)
                 flag = False
         if(len(error)>0):
             for err in error:
-                print("ERROR US_06: Check the Death Day at line number " + err + ". Divorce Date is after the death Day." )
+                us_rs.printError(err, "US 06", "Check the provided death date at the line. It occurs before the divorce.")
         return flag
                 
 
