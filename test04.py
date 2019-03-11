6c28a42315bdc181cb5001003b608753099739b1
Ashish Negi <ashishnegi2110@gmail.com>
2019-03-11 15:22:12 -0400

    conducted refactoring on user stories and also changed the input format for my test cases i.e. 4,7,21,33'

--- test04.py
+++ test04.py
@@ -4,29 +4,29 @@ from userstories_an import *
 class testcase(unittest.TestCase):
 
     def test_04(self):
-        result=userstory_an.us_04(['1994-06-12',4],['1994-04-03',6],'F1')
+        result=userstory_an.us_04([['1994-06-12',4],['1994-04-03',6],'F1'])
         self.assertEqual(result,"Error US04 in line 6 or line 4:Marriage date occurs after Divorce date in F1 family")
 
-        result=userstory_an.us_04('NA',['1994-04-03',6],'F1')
+        result=userstory_an.us_04(['NA',['1994-04-03',6],'F1'])
         self.assertEqual(result,0)
 
-        result=userstory_an.us_04(['1994-06-12',4],'NA','F1')
+        result=userstory_an.us_04([['1994-06-12',4],'NA','F1'])
         self.assertEqual(result,0)
 
-        result=userstory_an.us_04('NA','NA','F1')
+        result=userstory_an.us_04(['NA','NA','F1'])
         self.assertEqual(result,0)
 
-        result=userstory_an.us_04('NA',['Invalid',4],'F1')
+        result=userstory_an.us_04(['NA',['Invalid',4],'F1'])
         self.assertEqual(result,0)
 
-        result=userstory_an.us_04(['Invalid',6],['Invalid',8],'F1')
+        result=userstory_an.us_04([['Invalid',6],['Invalid',8],'F1'])
         self.assertEqual(result,0)
 
-        result=userstory_an.us_04(['1994-06-12',4],['Invalid',8],'F1')
+        result=userstory_an.us_04([['1994-06-12',4],['Invalid',8],'F1'])
         self.assertEqual(result,0)
 
-        result=userstory_an.us_04(['1977-12-06',4],['1980-12-08',6],'F2')
+        result=userstory_an.us_04([['1977-12-06',4],['1980-12-08',6],'F2'])
         self.assertEqual(result,0)
-
+        
 if __name__=='__main__':
     unittest.main()
--- userstories_an.py
+++ userstories_an.py
@@ -1,43 +1,43 @@
 class userstory_an():
 
-    def us_04(mar,div,i):   
-        if div!='NA' and mar!='NA' and mar[0]!='Invalid' and div[0]!='Invalid':
-            if mar[0]>div[0]:
-                return "Error US04 in line " + str(div[1]) +" or line "+ str(mar[1])+":Marriage date occurs after Divorce date in "+ i +" family"
+    def us_04(mar_div_details):
+        if mar_div_details[1]!= 'NA' and mar_div_details[0]!= 'NA' and mar_div_details[0][0]!= 'Invalid' and mar_div_details[1][0]!= 'Invalid':
+            if mar_div_details[0][0]>mar_div_details[1][0]:
+                return "Error US04 in line " + str(mar_div_details[1][1]) + " or line " + str(mar_div_details[0][1]) + ":Marriage date occurs after Divorce date in " + mar_div_details[2] + " family"
             else:
                 return 0
         else:
             return 0
 
-    def us_07(birth,death,Age,i):
-        if Age!='NA' and Age!='Invalid':
-            if Age>=150:
-                if death!='NA':
-                    return "Error US07 in line "+ str(birth[1]) +" or line "+ str(death[1])+":Age of person is 150 years with "+ i +" id"
+    def us_07(date):
+        if date[2]!= 'NA' and date[2]!= 'Invalid':
+            if date[2]>=150:
+                if date[1]!= 'NA':
+                    return "Error US07 in line " + str(date[0][1]) + " or line " + str(date[1][1]) + ":Age of person is 150 years with " + date[3] + " id"
                 else:
-                    return "Error US07 in line "+ str(birth[1]) +":Age of person is 150 years with "+ i +" id"
+                    return "Error US07 in line " + str(date[0][1]) + ":Age of person is 150 years with " + date[3] + " id"
             else:
                 return 0
         else:
             return 0
     
-    def us_21(a,b,i,j,k):
-        temp=[]#here temp is used for storing errors
-        if a[0]=='M'or a[0]=='NA':
-            temp.append(0)
+    def us_21(gender_details):
+        orphan_children=[]
+        if gender_details[0][0]== 'M'or gender_details[0][0]== 'NA':
+            orphan_children.append(0)
         else:
-            temp.append("Error US21 in line "+ str(a[1]) +":Gender of husband with "+str(j)+ " id is not male in "+ str(i)+" family")
-        if b[0]=='F' or b[0]=='NA':
-            temp.append(0)
+            orphan_children.append("Error US21 in line " + str(gender_details[0][1]) + ":Gender of husband with " + str(gender_details[3]) + " id is not male in " + str(gender_details[2]) + " family")
+        if gender_details[1][0]== 'F' or gender_details[1][0]== 'NA':
+            orphan_children.append(0)
         else:
-            temp.append("Error US21 in line "+ str(b[1]) +":Gender of wife with "+str(k)+ " id is not female in "+ str(i)+" family")
-        return temp
+            orphan_children.append("Error US21 in line " + str(gender_details[1][1]) + ":Gender of wife with " + str(gender_details[4]) + " id is not female in " + str(gender_details[2]) + " family")
+        return orphan_children
 
-    def us_33(hus,wif,chil_age,chil):
-        if hus!='NA'and wif!='NA':
-            if hus[0]!='Invalid' and wif[0]!='Invalid':
-                if chil_age!='NA' and chil_age!='Invalid' and chil_age<18:
-                    return chil
+    def us_33(orphan_details):
+        if orphan_details[0]!= 'NA'and orphan_details[1]!= 'NA':
+            if orphan_details[0][0]!= 'Invalid' and orphan_details[1][0]!= 'Invalid':
+                if orphan_details[2]!= 'NA' and orphan_details[2]!= 'Invalid' and orphan_details[2]<18:
+                    return orphan_details[3]
                 else:
                     return 0
             else:
@@ -46,49 +46,53 @@ class userstory_an():
             return 0
 
     def parse_data_04(family):
-        for x in family:#here x is family_id
-            answer=userstory_an.us_04(family[x]['MARR_DATE'],family[x]['DIV_DATE'],x)
-            if answer!=0:
-                print(answer)
+        for family_id in family:
+            input_list=[family[family_id]['MARR_DATE'],family[family_id]['DIV_DATE'],family_id]
+            output=userstory_an.us_04(input_list)
+            if output!=0:
+                print(output)
         return 0
 
     def parse_data_07(ind):
-        for x in ind:#here x is individual persons id
-            answer=userstory_an.us_07(ind[x]['BIRT_DATE'],ind[x]['DEAT_DATE'],ind[x]['AGE'],x)
-            if answer!=0:
-                print(answer)
+        for individual_id in ind:
+            list_input=[ind[individual_id]['BIRT_DATE'],ind[individual_id]['DEAT_DATE'],ind[individual_id]['AGE'],individual_id]
+            output=userstory_an.us_07(list_input)#refactored by shortening the list of parameters by passing a list.
+            if output!=0:
+                print(output)
         return 0
 
     def parse_data_21(ind,family):
-        for x in family:#here x is family_id
-            if ('HUSB' in family[x]) and (family[x]['HUSB'][0] in ind) and ('SEX' in ind[family[x]['HUSB'][0]]):
-                a=ind[family[x]['HUSB'][0]]['SEX']
-                c=family[x]['HUSB'][0]
+        for family_id in family:
+            if ('HUSB' in family[family_id]) and (family[family_id]['HUSB'][0] in ind) and ('SEX' in ind[family[family_id]['HUSB'][0]]):
+                husband_sex=ind[family[family_id]['HUSB'][0]]['SEX']#refactored by renaming variables
+                husband_ind_id=family[family_id]['HUSB'][0]#refactored by renaming variable
                 
             else:
-                a=['NA',-1]#a is husband's sex
-                c='NA'#c is hysband's id
+                husband_sex=['NA',-1]
+                husband_ind_id='NA'
             
-            if ('WIFE' in family[x]) and (family[x]['WIFE'][0] in ind) and ('SEX' in ind[family[x]['WIFE'][0]]):
-                b=ind[family[x]['WIFE'][0]]['SEX']
-                d=family[x]['WIFE'][0]
+            if ('WIFE' in family[family_id]) and (family[family_id]['WIFE'][0] in ind) and ('SEX' in ind[family[family_id]['WIFE'][0]]):
+                wife_sex=ind[family[family_id]['WIFE'][0]]['SEX']
+                wife_ind_id=family[family_id]['WIFE'][0]
                 
             else:
-                b=['NA',-1]#b is wife's sex
-                d='NA'#d is wife's id
-            answer=userstory_an.us_21(a,b,x,c,d)
-            for i in answer:
+                wife_sex=['NA',-1]
+                wife_ind_id='NA'
+            input_list=[husband_sex,wife_sex,family_id,husband_ind_id,wife_ind_id]
+            output=userstory_an.us_21(input_list)
+            for i in output:
                 if i!=0:
                     print(i)
         return 0
 
     def parse_data_33(ind,family):
-        orphan_chil=[]
-        for x in family:#here x is family_id
-            if ('HUSB' in family[x]) and ('WIFE' in family[x]) and ('CHIL' in family[x]) and (family[x]['HUSB'][0] in ind) and (family[x]['WIFE'][0] in ind):
-                for y in family[x]['CHIL']:#here y is children id in each family
-                    if (y[0] in ind):
-                        answer=userstory_an.us_33(ind[family[x]['HUSB'][0]]['DEAT_DATE'],ind[family[x]['WIFE'][0]]['DEAT_DATE'],ind[y[0]]['AGE'],y[0])
-                        if answer!=0:
-                            orphan_chil.append(answer)
-        print("List of id's of orpan children is: "+ str(orphan_chil))
+        orphan_children=[]
+        for family_id in family:
+            if ('HUSB' in family[family_id]) and ('WIFE' in family[family_id]) and ('CHIL' in family[family_id]) and (family[family_id]['HUSB'][0] in ind) and (family[family_id]['WIFE'][0] in ind):
+                for child_id in family[family_id]['CHIL']:
+                    if (child_id[0] in ind):
+                        input_list=[ind[family[family_id]['HUSB'][0]]['DEAT_DATE'],ind[family[family_id]['WIFE'][0]]['DEAT_DATE'],ind[child_id[0]]['AGE'],child_id[0]]
+                        output=userstory_an.us_33(input_list)
+                        if output!=0:
+                            orphan_children.append(output)
+        print("List of id's of orpan children is: "+ str(orphan_children))
