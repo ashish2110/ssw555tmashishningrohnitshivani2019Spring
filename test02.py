065a678a41bd17b67dfac11e758fcda12c381882
spatole12 <spatole@stevens.edu>
2019-03-11 18:33:07 -0400

    #Test case 02 changes

--- test02.py
+++ test02.py
@@ -1,11 +1,20 @@
 import unittest
 from us02_sp import us02_birth_is_before_marriage
+from us02_sp import us02_birth_before_marriage
 
 class TestProject(unittest.TestCase):
     
     """
     Birth should occur before marriage of an individual
     """
+
+    incorrect_ind = {
+        '@I1@': {'NAME': ['Adam /Levine/', 17], 'SEX': ['M', 21], 'BIRT_DATE': ['1964-02-25', 23], 'DEAT_DATE': ['2001-01-31', 25], 'FAMC': [['@F1@', 26]], 'ALIVE': 'False', 'FAMS': 'NA'},
+        '@I2@': {'NAME': ['Sherley /Johnson/', 17], 'SEX': ['F', 21], 'BIRT_DATE': ['2000-02-25', 23], 'DEAT_DATE': ['NA', 25], 'FAMC': [['@F1@', 26]], 'ALIVE': 'True', 'FAMS': 'NA'}
+        }
+
+    incorrect_family = {'@F1@': {'HUSB': ['@I1@', 139], 'WIFE': ['@I2@', 140], 'CHIL': [['NA']], 'MARR_DATE': ['1995-03-02', 143], 'DIV_DATE': ['1972-03-28', 153]}
+    }
     def test_us02_birth_is_before_marriage(self):
         # Assure that the dates are in properformat
         # template :  us02_birth_is_before_marriage(birth_date,marriage_date)
@@ -23,6 +32,9 @@ class TestProject(unittest.TestCase):
         # if birth date  is after the marriage date, it is false
         self.assertFalse(us02_birth_is_before_marriage("2019-04-13","2000-06-15"))
         
+        
+        #if there is an error in the user story, it will print the error and return false
+        self.assertFalse(us02_birth_before_marriage(self.incorrect_ind,self.incorrect_family))
 
 
 if __name__ == '__main__':
--- us02_sp.py
+++ us02_sp.py
@@ -22,6 +22,7 @@ def us02_birth_before_marriage(ind, family):
                                 isvalid = us02_birth_is_before_marriage(ind[key]["BIRT_DATE"][0],family[key1]["MARR_DATE"][0])
                                 if (isvalid == False):
                                     print('Error US02 in line',ind[key]["BIRT_DATE"][1],': Birth date of', ind[key]["NAME"][0], '(', key ,') occurs after the marriage date.')
+                                    return isvalid
                                 break
 
     
