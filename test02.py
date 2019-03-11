47df5dd9afd80f4b5c7706ac3f11e2c879359bdd
spatole12 <spatole@stevens.edu>
2019-03-11 18:52:20 -0400

    test changes

--- test02.py
+++ test02.py
@@ -9,8 +9,8 @@ class TestProject(unittest.TestCase):
     """
 
     incorrect_ind = {
-        '@I1@': {'NAME': ['Adam /Levine/', 17], 'SEX': ['M', 21], 'BIRT_DATE': ['1964-02-25', 23], 'DEAT_DATE': ['2001-01-31', 25], 'FAMC': [['@F1@', 26]], 'ALIVE': 'False', 'FAMS': 'NA'},
-        '@I2@': {'NAME': ['Sherley /Johnson/', 17], 'SEX': ['F', 21], 'BIRT_DATE': ['2000-02-25', 23], 'DEAT_DATE': ['NA', 25], 'FAMC': [['@F1@', 26]], 'ALIVE': 'True', 'FAMS': 'NA'}
+        '@I1@': {'NAME': ['Adam /Levine/', 17], 'SEX': ['M', 21], 'BIRT_DATE': ['1964-02-25', 23], 'DEAT_DATE': ['2001-01-31', 25], 'FAMC': [['@F1@', 26]], 'ALIVE': 'False', 'FAMS': [['@F1@', 26]]},
+        '@I2@': {'NAME': ['Sherley /Johnson/', 17], 'SEX': ['F', 21], 'BIRT_DATE': ['2000-02-25', 23], 'DEAT_DATE': ['NA', 25], 'FAMC': [['@F1@', 26]], 'ALIVE': 'True', 'FAMS': [['@F1@', 26]]}
         }
 
     incorrect_family = {'@F1@': {'HUSB': ['@I1@', 139], 'WIFE': ['@I2@', 140], 'CHIL': [['NA']], 'MARR_DATE': ['1995-03-02', 143], 'DIV_DATE': ['1972-03-28', 153]}
