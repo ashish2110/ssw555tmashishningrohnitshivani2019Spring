f7619e6c30a5e720ecd599ec4f61d464e37b208e
Ning YANG <yndlut@gmail.com>
2019-03-22 21:30:47 -0400

    User story #10 Includes refactoring result.

--- us10_ny.py
+++ us10_ny.py
@@ -15,16 +15,19 @@ def us10_tsk01_is_child_marriage(domString, husDOBString, wifeDOBString):
         dom = datetime.datetime.strptime(domString, '%Y-%m-%d')
         husDOB = datetime.datetime.strptime(husDOBString, '%Y-%m-%d')
         wifeDOB = datetime.datetime.strptime(wifeDOBString, '%Y-%m-%d')
-        # age to be refined
-        husAgeByDOM = int((dom-husDOB).days/365)
-        wifeAgeByDOM = int((dom-wifeDOB).days/365)
-        # if age is negative, it may be related to US 02 and it is considered False
-        if(husAgeByDOM < 0 or wifeAgeByDOM < 0):
+        # if DOM is before DOB, it may be related to US 02 and it is considered False
+        if(dom < husDOB or dom < wifeDOB):
             return False
-        elif(husAgeByDOM < 14 or wifeAgeByDOM < 14):
-            return True
         else:
-            return False
+            return (dates_within(dom, husDOB, 14, 'years') or dates_within(dom, wifeDOB, 14, 'years'))
     except ValueError:
         #if any input date is NA or invalid, it is considered as false
         return False
+
+# dt1, dt2 are instances of datetime
+# limit is a number
+# units is a string in ('days', 'months', 'years')
+# return True if dt1 and dt2 are within limit units
+def dates_within(dt1, dt2, limit, units):
+    conversion = {'days': 1, 'months': 30.4, 'years': 365.25}
+    return (abs((dt1 - dt2).days) / conversion[units]) <= limit
