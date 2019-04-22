040fce9aced156d91f0aef2db7c8f73b60f25f37
spatole12 <spatole@stevens.edu>
2019-04-22 12:36:12 -0400

    display us 17

--- userstories_sp.py
+++ userstories_sp.py
@@ -207,18 +207,22 @@ class userstories_sp():
             
 
     def us09_child_birth_parent_death_check(mother_death_date,father_death_date,child_birth_date):
+        # print(mother_death_date)
+        # print(father_death_date)
+        # print(child_birth_date)
         if not(userstories_sp.is_date_parsable(child_birth_date[0])):
             return True
         child_birth_date = userstories_sp.date_conversion(child_birth_date[0])
-        if ((mother_death_date == "NA" or mother_death_date == "Invalid") and father_death_date != "NA"):
+        if ((mother_death_date == "NA" or mother_death_date == "Invalid") and father_death_date != "NA" and father_death_date != "Invalid" and father_death_date[0] != "Invalid"):
             # print("debug 1")
+            # print(father_death_date)
             father_death_date = userstories_sp.date_conversion(father_death_date[0])
             return abs((child_birth_date-father_death_date).days/30.43) < 9 or father_death_date > child_birth_date
-        elif((father_death_date == "NA" or father_death_date == "Invalid" )and mother_death_date != "NA"):
+        elif((father_death_date == "NA" or father_death_date == "Invalid" )and mother_death_date != "NA" and mother_death_date != "Invalid" and mother_death_date[0] != "Invalid"):
             # print("debug 2")
             mother_death_date = userstories_sp.date_conversion(mother_death_date[0])
             return child_birth_date < mother_death_date
-        elif(father_death_date != "NA" and mother_death_date !="NA" and father_death_date != "Invalid" and mother_death_date !="Invalid" ):
+        elif(father_death_date != "NA" and mother_death_date !="NA" and father_death_date != "Invalid" and mother_death_date !="Invalid" and father_death_date[0] != "Invalid" and mother_death_date[0] !="Invalid" ):
             # print("debug 3")
             father_death_date = userstories_sp.date_conversion(father_death_date[0])
             mother_death_date = userstories_sp.date_conversion(mother_death_date[0])
